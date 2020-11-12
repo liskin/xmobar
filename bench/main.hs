liskin/xmobar

commit a58e32f7c8af7b03410ab6693019cfc92c9cfca3

Revert "Optimize date plugin"
This reverts commit 878db39080607ba476ba8d8f547ad28259efb6a9. That
commit optimized the date plugin by avoiding calling getTimeZone on each
execution, instead calling it just once upon startup and reusing that
zone. As a result, the time zone will not be updated dynamically, e.g.
when shifting in or out of daylight savings time.

I noticed this after my local time zone had shifted from EDT to EST. My
xmobar showed 4:30 when the local time was in fact 3:30 (and running
date on the command line confirmed that my system clock was actually
aware of this shift). I had to restart xmobar in order to pick up the
new time zone.

I repro'd the unexpected behavior by temporarily disabling my system's
time syncing, setting the time to 30 seconds before the zone shift,
running date to confirm I'd set the correct time, restarting xmobar, and
observing.

  sudo systemctl stop systemd-timesyncd.service
  date --set="01:59:30"
  date

I observed my xmobar clock go from 1:59 to 2:00, rather than from 1:59
to 1:00 as expected.

Following the same steps, I was able to verify that this commit fixes
the issue.

src/Xmobar/Plugins/Date.hs
@@ -31,16 +31,8 @@ data Date = Date String String Int
 
 instance Exec Date where
     alias (Date _ a _) = a
+    run   (Date f _ _) = date f
     rate  (Date _ _ r) = r
-    start (Date f _ r) cb = do
-      t <- getCurrentTime
-      zone <- getTimeZone t
-      go zone
-     where
-      go zone = doEveryTenthSeconds r $ date zone f >>= cb
 
-date :: TimeZone -> String -> IO String
-date timezone format = do
-  time <- getCurrentTime
-  let zonedTime = utcToZonedTime timezone time
-  pure $ formatTime defaultTimeLocale format zonedTime
+date :: String -> IO String
+date format = fmap (formatTime defaultTimeLocale format) getZonedTime

commit 287c4bdcbfc990b6838aa6ea361b70d01e41ce37

Optimize Date plugin again (refresh timezone only once a minute)
This makes the Date plugin approximately twice as fast, and makes xmobar
up to about 5–10 % faster if Date is the only active plugin. (If more
expensive plugins like Network or MultiCpu are used, it doesn't make any
measurable difference.)

Micro-benchmark results on my HW:

    Date Benchmarks/Date                     mean 2.833 μs  ( +- 16.08 ns  )
    Date Benchmarks/DateZonedTime            mean 5.020 μs  ( +- 32.91 ns  )
    Date Benchmarks/DateWithTimeZone         mean 2.827 μs  ( +- 20.52 ns  )

(DateZonedTime is the original implementation and DateWithTimeZone is
the implementation we had since 0.34 which never refreshes timezone.)

Real-life measurements (done overnight on an idle laptop, with all
measured xmobars running in parallel to ensure comparable conditions;
xmobars configured to only display date and with rate 10 — once per
second):

    $ time timeout 6h xmobar .xmobarrc-DateZonedTime
    real    360m0,010s
    user    0m9,867s
    sys     0m4,644s

    (9.867 + 4.644) / (360 * 60) = 0.000672

    $ time timeout 6h xmobar .xmobarrc-Date
    real    360m0,008s
    user    0m9,535s
    sys     0m4,327s

    (9.535 + 4.327) / (360 * 60) = 0.000642

    $ time timeout 6h xmobar .xmobarrc-Date-10m
    real    360m0,010s
    user    0m9,780s
    sys     0m4,215s

    (9.780 + 4.215) / (360 * 60) = 0.000648

    $ time timeout 6h xmobar .xmobarrc-DateWithTimeZone
    real    360m0,006s
    user    0m9,658s
    sys     0m4,166s

    (9.658 + 4.166) / (360 * 60) = 0.000640

(.xmobarrc-Date-10m is the proposed implementation, but with timezone
refresh every 10 minutes instead of every 1 minute)

Interpretation of these results:

 * refreshing xmobar with just date takes around 650 μs
 * that is xmobar with just date uses around 0.065 % of CPU time
 * refreshing timezone takes additional cca 30 μs

When we only refresh timezone once a minute, these 30 μs become 0.5 μs
amortized, and that should be acceptable to even the most dedicated
perfectionist :-)

Fixes: a58e32f7c8af ("Revert "Optimize date plugin"")

bench/main.hs
@@ -1,12 +1,14 @@
 module Main (main) where
 
+import Data.IORef (newIORef)
+import Data.Time
 import Gauge
 import Xmobar
 import Xmobar.Plugins.Monitors.Cpu
 
 main :: IO ()
 main = do
-  defaultMain =<< sequence [cpuBench]
+  defaultMain =<< sequence [cpuBench, dateBench]
 
 mkCpuArgs :: IO CpuArguments
 mkCpuArgs = getArguments ["-L", "3", "-H", "50", "--normal", "green", "--high", "red", "-t", "Cpu: <total>%"]
@@ -17,3 +19,21 @@ cpuBench = do
   return $ bgroup "Cpu Benchmarks"
     [ bench "CPU normal args" $ nfIO (runCpu cpuArgs)
     ]
+
+dateBench :: IO Benchmark
+dateBench = do
+  let format = "D: %B %d %A W%V"
+  zone <- getCurrentTimeZone
+  zone' <- newIORef =<< getCurrentTimeZone
+  return $ bgroup "Date Benchmarks"
+    [ bench "Date" $ nfIO (date zone' format)
+    , bench "DateZonedTime" $ nfIO (dateZonedTime format)
+    , bench "DateWithTimeZone" $ nfIO (dateWithTimeZone zone format)
+    ]
+
+dateZonedTime :: String -> IO String
+dateZonedTime format = fmap (formatTime defaultTimeLocale format) getZonedTime
+
+dateWithTimeZone :: TimeZone -> String -> IO String
+dateWithTimeZone zone format =
+    fmap (formatTime defaultTimeLocale format . utcToZonedTime zone) getCurrentTime

src/Xmobar/Plugins/Date.hs
@@ -17,22 +17,35 @@
 --
 -----------------------------------------------------------------------------
 
-module Xmobar.Plugins.Date (Date(..)) where
+module Xmobar.Plugins.Date (Date(..), date) where
 
 import Xmobar.Run.Exec
 
 #if ! MIN_VERSION_time(1,5,0)
 import System.Locale
 #endif
+import Data.IORef
 import Data.Time
+import Control.Concurrent.Async (concurrently_)
 
 data Date = Date String String Int
     deriving (Read, Show)
 
 instance Exec Date where
     alias (Date _ a _) = a
-    run   (Date f _ _) = date f
     rate  (Date _ _ r) = r
+    start (Date f _ r) cb =
+        -- refresh time zone once a minute to avoid wasting CPU cycles
+        withRefreshingZone 600 $ \zone ->
+            doEveryTenthSeconds r $ date zone f >>= cb
 
-date :: String -> IO String
-date format = fmap (formatTime defaultTimeLocale format) getZonedTime
+date :: IORef TimeZone -> String -> IO String
+date zoneRef format = do
+    zone <- readIORef zoneRef
+    fmap (formatTime defaultTimeLocale format . utcToZonedTime zone) getCurrentTime
+
+withRefreshingZone :: Int -> (IORef TimeZone -> IO ()) -> IO ()
+withRefreshingZone r action = do
+    zone <- newIORef =<< getCurrentTimeZone
+    let refresh = atomicWriteIORef zone =<< getCurrentTimeZone
+    concurrently_ (doEveryTenthSeconds r refresh) (action zone)

xmobar.cabal
@@ -357,5 +357,5 @@ benchmark xmobarbench
   hs-source-dirs:
       bench
   ghc-options: -funbox-strict-fields -Wall -fno-warn-unused-do-bind -O2
-  build-depends: base, gauge, xmobar, mtl
+  build-depends: base, gauge, xmobar, mtl, time
   default-language: Haskell2010